9f89f77a5eb26b29826ea3cd4c14542c624ad36d
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-01-27 11:11:16 -0500

    Make NMS cross class boundaries

--- csrc/cuda/nms.cu
+++ csrc/cuda/nms.cu
@@ -52,24 +52,20 @@ __global__ void nms_kernel(
       if (i < num_detections && m < i && scores[m] > 0.0f) {
         int idx = indices[i];
         int max_idx = indices[m];
-        int icls = classes[idx];
-        int mcls = classes[max_idx];
-        if (mcls == icls) {
-          float4 ibox = boxes[idx];
-          float4 mbox = boxes[max_idx];
-          float x1 = max(ibox.x, mbox.x);
-          float y1 = max(ibox.y, mbox.y);
-          float x2 = min(ibox.z, mbox.z);
-          float y2 = min(ibox.w, mbox.w);
-          float w = max(0.0f, x2 - x1 + 1);
-          float h = max(0.0f, y2 - y1 + 1);
-          float iarea = (ibox.z - ibox.x + 1) * (ibox.w - ibox.y + 1);
-          float marea = (mbox.z - mbox.x + 1) * (mbox.w - mbox.y + 1);
-          float inter = w * h;
-          float overlap = inter / (iarea + marea - inter);
-          if (overlap > threshold) {
-            scores[i] = 0.0f;
-          }
+        float4 ibox = boxes[idx];
+        float4 mbox = boxes[max_idx];
+        float x1 = max(ibox.x, mbox.x);
+        float y1 = max(ibox.y, mbox.y);
+        float x2 = min(ibox.z, mbox.z);
+        float y2 = min(ibox.w, mbox.w);
+        float w = max(0.0f, x2 - x1 + 1);
+        float h = max(0.0f, y2 - y1 + 1);
+        float iarea = (ibox.z - ibox.x + 1) * (ibox.w - ibox.y + 1);
+        float marea = (mbox.z - mbox.x + 1) * (mbox.w - mbox.y + 1);
+        float inter = w * h;
+        float overlap = inter / (iarea + marea - inter);
+        if (overlap > threshold) {
+          scores[i] = 0.0f;
         }
       }
     }
